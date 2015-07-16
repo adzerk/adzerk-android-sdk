667121b0b3810ab022cb91d11a084669351144c5
Betty Diegel <bdiegel@gmail.com>
2015-07-16 11:45:00 -0400

    Add builder for constructing Request objects
    - Request constructor is private
    - made setPlacements private
    - left other setters public for now

--- sdk/src/main/java/com/adzerk/android/sdk/rest/Request.java
+++ sdk/src/main/java/com/adzerk/android/sdk/rest/Request.java
@@ -13,7 +13,7 @@ import java.util.Set;
  */
 public class Request {
 
-    // list pf placements where an ad can be served (required)
+    // list of placements where an ad can be served (required)
     private List<Placement> mPlacements;
 
     // target to the user key used to identify a unique user
@@ -41,13 +41,171 @@ public class Request {
     private Map<Integer, List<Integer>> mFlightViewTimes;
 
     // if true, only ads containing a single image will be returned (defaults to false)
-    private Boolean mIsMobile;
+    private Boolean mIsMobile = Boolean.FALSE;
+
+    /**
+     * Builder used to create Request for ads.
+     */
+    public static class Builder {
+
+        private List<Placement> placements;
+        private User user;
+        private Set<String> keywords;
+        private URL referrer;
+        private URL url;
+        private Long time;
+        private InetAddress ip;
+        private Set<Integer> blockedCreatives;
+        private Map<Integer, List<Integer>> flightViewTimes;
+        private boolean isMobile = false;
+
+        /**
+         * Builder to configure a Request for Ads.
+         *
+         * Multiple ads can be served by a single Request. Placements identify locations in the app where an ad can
+         * be served. And each placement defines separate properties for a requested ad.
+         *
+         * <pre>
+         * {@code
+         * Request request = Request.Builder(placements)
+         *     .setUser(user)
+         *     .setMobile(true)
+         *     .build()
+         * }
+         * </pre>
+         *
+         * @param placements list of placements where an ad can be served (required)
+         */
+        public Builder(List<Placement> placements) {
+            if (placements == null || placements.isEmpty())
+                throw new IllegalArgumentException("At least one Placement must be specified");
+            this.placements = placements;
+        }
+
+        /**
+         * User to target. If the request doesn't contain a user key, one will be automatically generated
+         * in the response.
+         *
+         * @param user  user identified by a unique key
+         * @return request builder
+         */
+        public Builder setUser(User user) {
+            this.user = user;
+            return this;
+        }
+
+        /**
+         * Keywords used when selecting the ad
+         *
+         * @param keywords  zero or more keywords
+         * @return request builder
+         */
+        public Builder setKeywords(Set<String> keywords) {
+            this.keywords = keywords;
+            return this;
+        }
+
+        /**
+         * URL used as referrer when selecting an ad
+         *
+         * @param referrer  referrer URL
+         * @return request builder
+         */
+        public Builder setReferrer(URL referrer) {
+            this.referrer = referrer;
+            return this;
+        }
+
+        /**
+         * URL to use as the current page URL when selecting an ad
+         * @param url   URL of current page
+         * @return request builder
+         */
+        public Builder setUrl(URL url) {
+            this.url = url;
+            return this;
+        }
+
+        /**
+         * Timestamp to use when selecting an ad
+         * @param time  UNIX epoch timestamp
+         * @return request builder
+         */
+        public Builder setTime(long time) {
+            this.time = time;
+            return this;
+        }
+
+        /**
+         * IP address to use when selecting the ad. If specified, overrides the IP the request is made from
+         * @param ip    ip address
+         * @return request builder
+         */
+        public Builder setIP(InetAddress ip) {
+            this.ip = ip;
+            return this;
+        }
+
+        /**
+         * Creative ids to disregard when selecting an ad
+         * @param blockedCreatives  zero or more numeric creative ids to disregard
+         * @return request builder
+         */
+        public Builder setBlockedCreatives(Set<Integer> blockedCreatives) {
+            this.blockedCreatives = blockedCreatives;
+            return this;
+        }
+
+        /**
+         * Map of flight ids to list of timestamps representing times the user viewed an ad in the specified
+         * flight. Used for frequency capping.
+         *
+         * @param flightViewTimes   map of flight ids to list of UNIX epoch timestamps
+         * @return request builder
+         */
+        public Builder setFlightViewTimes(Map<Integer, List<Integer>> flightViewTimes) {
+            this.flightViewTimes = flightViewTimes;
+            return this;
+        }
+
+        /**
+         * If true, only ads containing a single image will be returned (defaults to false)
+         *
+         * @param isMobile  set to true to request single image ads
+         * @return request builder
+         */
+        public Builder setMobile(boolean isMobile) {
+            this.isMobile = isMobile;
+            return this;
+        }
+
+        /**
+         * Create the Request
+         *
+         * @return ad request
+         */
+        public Request build() {
+            return new Request(this);
+        }
+
+    }
+
+    private Request(Builder builder) {
+        setPlacements(builder.placements);
+        setUser(builder.user);
+        setKeywords(builder.keywords);
+        setReferrer(builder.referrer);
+        setUrl(builder.url);
+        setTime(builder.time);
+        setIp(builder.ip);
+    }
+
 
     public List<Placement> getPlacements() {
         return mPlacements;
     }
 
-    public void setPlacements(List<Placement> placements) {
+    private void setPlacements(List<Placement> placements) {
         mPlacements = placements;
     }
 
